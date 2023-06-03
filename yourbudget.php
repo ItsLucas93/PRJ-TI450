ad128c016f6d4f83c99c72e8e3f5661beb857665
Lucas <45604254+ItsLucas93@users.noreply.github.com>
2023-06-03 17:47:17 +0200

    Update yourbudget.php

--- yourbudget.php
+++ yourbudget.php
@@ -40,81 +40,104 @@ session_start();
     </nav>
 </header>
 
-<div class="container_2">
+  <div class="container_2">
     <div class="left">
-        <section id="form">
-            <h2>Entrez vos dépenses mensuelles</h2>
-            <form onsubmit="recup_depenses(event);">
+      <section id="form">
+          <h2>Entrez vos dépenses mensuelles</h2>
+          <form onsubmit="recup_depenses(event);">
 
-                <label for="loyer">Loyer :</label>
-                <input type="number" id="loyer" name="loyer" required><br>
+              <label for="loyer">Loyer :</label>
+              <input type="number" id="loyer" name="loyer" required><br>
 
-                <label for="services_publics">Services publics :</label>
-                <input type="number" id="services_publics" name="services_publics" required><br>
+        <label for="services_publics">Services publics :</label>
+              <input type="number" id="services_publics" name="services_publics" required><br>
 
-                <label for="alimentation">Alimentation :</label>
-                <input type="number" id="alimentation" name="alimentation" required><br>
+              <label for="alimentation">Alimentation :</label>
+              <input type="number" id="alimentation" name="alimentation" required><br>
 
-                <label for="hygiene">Hygiène :</label>
-                <input type="number" id="hygiene" name="hygiene" required><br>
+        <label for="hygiene">Hygiène :</label>
+              <input type="number" id="hygiene" name="hygiene" required><br>
 
-                <label for="abonnements">Abonnement(s) :</label>
-                <input type="number" id="abonnements" name="abonnements" required><br>
+        <label for="abonnements">Abonnement(s) :</label>
+              <input type="number" id="abonnements" name="abonnements" required><br>
 
-                <label for="assurances">Assurance(s) :</label>
-                <input type="number" id="assurances" name="assurances" required><br>
+        <label for="assurances">Assurance(s) :</label>
+              <input type="number" id="assurances" name="assurances" required><br>
 
-                <label for="transport">Transport(s) :</label>
-                <input type="number" id="transport" name="transport" required><br>
+              <label for="transport">Transport(s) :</label>
+              <input type="number" id="transport" name="transport" required><br>
 
-                <label for="divertissement">Divertissement :</label>
-                <input type="number" id="divertissement" name="divertissement" required><br>
+              <label for="divertissement">Divertissement :</label>
+              <input type="number" id="divertissement" name="divertissement" required><br>
 
-                <label for="remboursement">Remboursement(s) :</label>
-                <input type="number" id="remboursement" name="remboursement" required><br>
+        <label for="autre_depenses">Autre dépenses :</label>
+              <input type="number" id="autre_depenses" name="autre_depenses" required><br>
 
-                <label for="autre_depenses">Autre dépenses :</label>
-                <input type="number" id="autre_depenses" name="autre_depenses" required><br>
+              <input type="submit" value="Calculer">
+          </form>
+      <p id="totalDepenses"></p>
+      </section>
+    <canvas id="depensesChart"></canvas>
 
-                <input type="submit" value="Calculer">
-            </form>
-            <p id="totalDepenses"></p>
-        </section>
-        <canvas id="myChart"></canvas>
+  </div>
 
+  <div class="right">
+  <section id="form">
+    <h2>Entrez vos revenus mensuels</h2>
+    <form onsubmit="recup_revenus(event);">
+
+      <label for="salaire">Salaire net :</label>
+      <input type="number" id="salaire" name="salaire" required><br>
+
+      <label for="aides_sociales">Aides sociales :</label>
+      <input type="number" id="aides_sociales" name="aides_sociales" required><br>
+
+      <label for="bourse">Bourse :</label>
+      <input type="number" id="bourse" name="bourse" required><br>
+
+      <label for="investissement">Investissement(s) :</label>
+      <input type="number" id="investissement" name="investissement" required><br>
+
+      <label for="locatif">Locatif :</label>
+      <input type="number" id="locatif" name="locatif" required><br>
+
+      <label for="autre_revenus">Autre revenus :</label>
+            <input type="number" id="autre_revenus" name="autre_revenus" required><br>
+
+      <input type="submit" value="Calculer">
+
+    </form>
+  </section>
+  <canvas id="revenuesChart"></canvas>
+
+</div>
+<div id="total"></div>
+
+</div>
+
+
+<canvas id="myChart"></canvas>
+
+<canvas id="repereRevenusChart"></canvas>
+
+<canvas id="repereDepensesChart"></canvas>
+
+<canvas id="differencesRevenusChart"></canvas>
+
+
+
+<footer>
+
+    <p>Suivez-nous sur les réseaux :</p>
+    <div class="social-media-icons">
+        <a href="https://www.facebook.com/EfreiParis/?locale=fr_FR" target="_blank"><i class="fab fa-facebook-f"></i></a>
+        <a href="https://twitter.com/Efrei_Paris" target="_blank"><i class="fab fa-twitter"></i></a>
+        <a href="https://www.instagram.com/efrei_paris" target="_blank"><i class="fab fa-instagram"></i></a>
     </div>
 
-    <div class="right">
-        <section id="form">
-            <h2>Entrez vos revenus mensuels</h2>
-            <form onsubmit="recup_revenus(event);">
 
-                <label for="salaire">Salaire net :</label>
-                <input type="number" id="salaire" name="salaire" required><br>
-
-                <label for="aides_sociales">Aides sociales :</label>
-                <input type="number" id="aides_sociales" name="aides_sociales" required><br>
-
-                <label for="bourse">Bourse :</label>
-                <input type="number" id="bourse" name="bourse" required><br>
-
-                <label for="investissement">Investissement(s) :</label>
-                <input type="number" id="investissement" name="investissement" required><br>
-
-                <label for="locatif">Locatif :</label>
-                <input type="number" id="locatif" name="locatif" required><br>
-
-                <label for="autre_revenus">Autre revenus :</label>
-                <input type="number" id="autre_revenus" name="autre_revenus" required><br>
-
-                <input type="submit" value="Calculer">
-
-            </form>
-        </section>
-        <canvas id="revenuesChart"></canvas>
-
-    </div>
-    <div id="total"></div>
+    <p>&copy; 2023 S_Budget - <a href="https://github.com/ItsLucas93" target="_blank" style="color: inherit; text-decoration: underline; text-decoration-color: white;">Tous droits réservés</a>.</p>
+</footer>
 
 </body>
 </html>
